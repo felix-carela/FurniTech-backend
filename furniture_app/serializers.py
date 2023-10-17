99e1f56e423cac06292f50bc4f2c69433fd03da3
felix-carela <felixacarela@protonmail.com>
2023-10-17 17:06:35 -0400

    Implemented users

--- furniture_app/serializers.py
+++ furniture_app/serializers.py
@@ -1,16 +1,22 @@
-# from django.contrib.auth.models import Item
-# from rest_framework import serializers
-
-# class ItemSerializer(serializers.HyperlinkedModelSerializer):
-#     class Meta:
-#         model = Item
-#         fields = '__all__'
-
 from django.contrib.auth.models import User
 from rest_framework import serializers
-
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = ['url', 'username', 'email', 'password']
+        fields = ['url', 'username', 'email']
+
+class UserCreateSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password']
+        )
+        return user
--- furniture_app/views.py
+++ furniture_app/views.py
@@ -1,13 +1,42 @@
 from django.contrib.auth.models import User
-from rest_framework import viewsets
-from rest_framework import permissions
-from furniture_app.serializers import UserSerializer
-
+from rest_framework import viewsets, serializers
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from django.contrib.auth import login, logout, authenticate
+from .serializers import UserSerializer, UserCreateSerializer
 
 class UserViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows users to be viewed or edited.
-    """
-    queryset = User.objects.all().order_by('-date_joined')
+    queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = [permissions.IsAuthenticated]
+
+@api_view(['POST'])
+def signup_view(request):
+    if request.method == 'POST':
+        serializer = UserCreateSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            return Response({"message": "User created successfully!"}, status=201)
+        return Response(serializer.errors, status=400)
+
+@api_view(['POST'])
+def login_view(request):
+    username = request.data.get('username')
+    password = request.data.get('password')
+    user = authenticate(username=username, password=password)
+    if user:
+        login(request, user)
+        return Response({"message": "Logged in successfully!"})
+    return Response({"error": "Invalid credentials"}, status=400)
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def logout_view(request):
+    logout(request)
+    return Response({"message": "Logged out successfully!"})
+
+@api_view(['DELETE'])
+@permission_classes([IsAuthenticated])
+def delete_user_view(request):
+    request.user.delete()
+    return Response({"message": "User deleted successfully!"})
