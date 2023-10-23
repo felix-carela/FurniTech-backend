c032b1a48adaca2c1be4397f5a0bff4c57ffdb74
felix-carela <felixacarela@protonmail.com>
2023-10-23 08:57:25 -0400

    Add deployed Vercel

--- FurniTech_project/settings.py
+++ FurniTech_project/settings.py
@@ -193,6 +193,7 @@ CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
 CORS_ORIGIN_WHITELIST = [
     'http://localhost:3000',
+    'https://furni-tech-frontend-fb8jc20wu-gerardoap22.vercel.app/',
 ]
 
 django_heroku.settings(locals())
